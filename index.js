4f0765ca65fafeb65d1e955bcfb588147ef3b5b4
PJ Joshi <joshipv2@gmail.com>
2019-03-28 21:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don\'t have a credit card on file for you.`
   } else {
      cart.length = 0
-     return `Your total cost is $${cart.totValue}, which will be charged to the card ${cardNumber}.`
+     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
