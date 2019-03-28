ee352335c0789f723e6f29e952a753e95a080c17
PJ Joshi <joshipv2@gmail.com>
2019-03-28 18:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length-1; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i)
+      cart.splice(i, 1)
     } else {
       return 'That item is not in your cart.'
     }
