ac8386fb60f6e17c61f4772fbcbd94fc8d9c2cf1
PJ Joshi <joshipv2@gmail.com>
2019-03-28 21:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function viewCart() {
   if (cart.length === 0) {
       return `Your shopping cart is empty.`
   } else if (cart.length === 1) {
-    
-  
+        cartSt += `${cartItms}`
   }
   else {
-    
           for (let i = 0; i < cart.length; i++) {
               
                 if (i === cart.length-1) {
